37def816d88cad66c3164f93baff702558ff916f
Base <base@Baseui-MacBookAir.local>
2023-11-14 13:02:23 +0900

    vercel update & build

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana, Nanum_Brush_Script } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({weight: ['400'], subsets: ['latin']})
+export const nanumBrush = Nanum_Brush_Script({weight: ['400'], subsets: ['latin'] });
