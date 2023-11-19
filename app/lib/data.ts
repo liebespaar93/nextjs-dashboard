f39c18d1ffbfbf65fe7981e8606213ff57c25491
Base <base@Baseui-MacBookAir.local>
2023-11-19 22:30:38 +0900

    error done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -151,6 +151,7 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
+  
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -168,6 +169,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
